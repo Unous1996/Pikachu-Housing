f0ee79e317635c5372420bc11bf4f1da63362faf
Ao Li <aoli1996@berkeley.edu>
2018-11-21 15:41:09 -0600

    Nov21-1541

--- housing/api/serializers.py
+++ housing/api/serializers.py
@@ -3,18 +3,21 @@ from housing.models import House
 from distance.models import Distance
 from rest_framework.response import Response
 from django.http import JsonResponse
-from department.api.serializers import DepartmentSerializer
+from department.api.serializers import DepartmentSerializer, DepartmentSerializerWithDistance
+
 class HouseSerializer(serializers.ModelSerializer):
 
     closest_department = serializers.SerializerMethodField()
 
     def get_closest_department(self, obj):
+
         distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
+        
         for item in distance_set:
-            dept = item.department_id
-            serializer = DepartmentSerializer(dept)
-            return serializer.data
-        return 
+            serializer = DepartmentSerializer(item.department_id)
+            serialized_data = serializer.data
+            serialized_data['distance'] = item.distance
+            return serialized_data
 
     class Meta:
         model = House
