e3864a11f702ec2f2a5ac2e33b076d38c1898f10
Ao Li <aoli1996@berkeley.edu>
2018-11-21 14:38:51 -0600

    Serialized department

--- department/api/views.py
+++ department/api/views.py
@@ -5,21 +5,18 @@ from serializers import DepartmentSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import permission_classes
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-
+from rest_framework import permissions
 
 @permission_classes((IsAuthenticatedOrReadOnly,))
 class DepartmentViewSet(viewsets.ModelViewSet):
     # pagination_class = DepartmentPagination
+    pagination_class = DepartmentPagination
     serializer_class = DepartmentSerializer
-    queryset = ''
+    permission_classes = (permissions.BasePermission,)
 
-    def list(self, request):
-        queryset = Department.objects.raw('SELECT * FROM department_department')
-        serializers = DepartmentSerializer(queryset, many=True)
-        return Response(serializers.data)
-
-    def retrieve(self, request, pk):
-        queryset = Department.objects.raw('SELECT * FROM department_department WHERE department_department.id = %s',[pk])
-        serializers = DepartmentSerializer(queryset, many=True)
-        return Response(serializers.data)
-
+    def get_queryset(self):
+        queryset = Department.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
--- housing/api/serializers.py
+++ housing/api/serializers.py
@@ -1,22 +1,20 @@
 from rest_framework import serializers
 from housing.models import House
 from distance.models import Distance
+from rest_framework.response import Response
+from django.http import JsonResponse
+from department.api.serializers import DepartmentSerializer
 class HouseSerializer(serializers.ModelSerializer):
 
-    closest_department_id = serializers.SerializerMethodField()
-    closest_department_name = serializers.SerializerMethodField()
+    closest_department = serializers.SerializerMethodField()
 
-    def get_closest_department_id(self, obj):
+    def get_closest_department(self, obj):
         distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
         for item in distance_set:
-            return item.department_id.id
-        return None
-
-    def get_closest_department_name(self, obj):
-        distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
-        for item in distance_set:
-            return item.department_id.name
-        return None
+            dept = item.department_id
+            serializer = DepartmentSerializer(dept)
+            return serializer.data
+        return 
 
     class Meta:
         model = House
@@ -32,6 +30,5 @@ class HouseSerializer(serializers.ModelSerializer):
             'latitude',
             'longitude',
             'provider',
-            'closest_department_id',
-            'closest_department_name'
+            'closest_department',
         )
