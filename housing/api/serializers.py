54e77467bbc1938b54161ef0508371741ad33446
Ao Li <aoli1996@berkeley.edu>
2018-11-21 03:27:57 -0600

    Add dept id and name

--- housing/api/serializers.py
+++ housing/api/serializers.py
@@ -3,12 +3,19 @@ from housing.models import House
 from distance.models import Distance
 class HouseSerializer(serializers.ModelSerializer):
 
-    closest_department = serializers.SerializerMethodField()
+    closest_department_id = serializers.SerializerMethodField()
+    closest_department_name = serializers.SerializerMethodField()
 
-    def get_closest_department(self, obj):
+    def get_closest_department_id(self, obj):
         distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
         for item in distance_set:
             return item.department_id.id
+        return None
+
+    def get_closest_department_name(self, obj):
+        distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
+        for item in distance_set:
+            return item.department_id.name
         return None
 
     class Meta:
@@ -25,5 +32,6 @@ class HouseSerializer(serializers.ModelSerializer):
             'latitude',
             'longitude',
             'provider',
-            'closest_department'
+            'closest_department_id',
+            'closest_department_name'
         )
