bc116c1e1907b0d7d503c238eda0bb87b35c1aa7
Ao Li <aoli1996@berkeley.edu>
2018-11-24 17:28:06 -0600

    Nov24_1728

--- users/api/serializers.py
+++ users/api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from users.models import UserProfile
 from django.contrib.auth.models import User
-
+from housing.models import House
+from department.models import Department
+from department.api.serializers import DepartmentSerializer
+from distance.models import Distance
+from like.models import Like
 
 class UserProfileSerializer(serializers.ModelSerializer):
     viewed_house = serializers.SerializerMethodField()
@@ -16,6 +20,137 @@ class UserProfileSerializer(serializers.ModelSerializer):
     def get_viewed_house(self, obj):
         return [house.name for house in obj.viewed_houses.all()]
 
+class UserHouseSerializer12(serializers.ModelSerializer):
+    like_count = serializers.SerializerMethodField()
+    closest_department = serializers.SerializerMethodField()
+    reason = serializers.SerializerMethodField()
+
+    def get_like_count(self, obj):
+        count = Like.objects.filter(house_id = obj.id).count()
+        return count
+
+    def get_closest_department(self, obj):
+        department_set = Department.objects.raw('SELECT * FROM department_department WHERE id = %s', [int(obj.closest_department_float)])
+        distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
+
+        for item in department_set:
+            serializer = DepartmentSerializer(item)
+            serialized_data = serializer.data
+
+        for item in distance_set:
+            serialized_data['distance'] = item.distance
+            return serialized_data
+
+    def get_reason(self, obj):
+        if(self.get_like_count(obj)>=3):
+            return ('close to department','cheap house', 'hot house')
+        else:
+            return ('close to department','cheap house')
+
+    class Meta:
+        model = House
+        fields = (
+            'id',
+            'name',
+            'price',
+            'location',
+            'cover_img',
+            'types',
+            'description',
+            'imgs_url',
+            'latitude',
+            'longitude',
+            'provider',
+            'closest_department',
+            'like_count',
+            'reason'
+        )
+
+class UserHouseSerializer13(serializers.ModelSerializer):
+    like_count = serializers.SerializerMethodField()
+    closest_department = serializers.SerializerMethodField()
+    reason = serializers.SerializerMethodField()
+
+    def get_like_count(self, obj):
+        count = Like.objects.filter(house_id = obj.id).count()
+        return count
+
+    def get_closest_department(self, obj):
+        department_set = Department.objects.raw('SELECT * FROM department_department WHERE id = %s', [int(obj.closest_department_float)])
+        distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
+
+        for item in department_set:
+            serializer = DepartmentSerializer(item)
+            serialized_data = serializer.data
+
+        for item in distance_set:
+            serialized_data['distance'] = item.distance
+            return serialized_data
+
+    def get_reason(self, obj):
+        return ('close to department', 'hot house')
+
+    class Meta:
+        model = House
+        fields = (
+            'id',
+            'name',
+            'price',
+            'location',
+            'cover_img',
+            'types',
+            'description',
+            'imgs_url',
+            'latitude',
+            'longitude',
+            'provider',
+            'closest_department',
+            'like_count',
+            'reason'
+        )
+
+class UserHouseSerializer23(serializers.ModelSerializer):
+    like_count = serializers.SerializerMethodField()
+    closest_department = serializers.SerializerMethodField()
+    reason = serializers.SerializerMethodField()
+
+    def get_like_count(self, obj):
+        count = Like.objects.filter(house_id = obj.id).count()
+        return count
+
+    def get_closest_department(self, obj):
+        department_set = Department.objects.raw('SELECT * FROM department_department WHERE id = %s', [int(obj.closest_department_float)])
+        distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
+
+        for item in department_set:
+            serializer = DepartmentSerializer(item)
+            serialized_data = serializer.data
+
+        for item in distance_set:
+            serialized_data['distance'] = item.distance
+            return serialized_data
+
+    def get_reason(self, obj):
+        return ('cheap house', 'hot house')
+
+    class Meta:
+        model = House
+        fields = (
+            'id',
+            'name',
+            'price',
+            'location',
+            'cover_img',
+            'types',
+            'description',
+            'imgs_url',
+            'latitude',
+            'longitude',
+            'provider',
+            'closest_department',
+            'like_count',
+            'reason'
+        )
 
 class UserSerializer(serializers.ModelSerializer):
     id = serializers.ReadOnlyField()
--- users/api/views.py
+++ users/api/views.py
@@ -1,8 +1,8 @@
-from rest_framework import viewsets,status
+from rest_framework import viewsets,status, serializers
 from users.models import UserProfile
 from django.contrib.auth.models import User, BaseUserManager
 from users.api.paginations import UserPagination
-from users.api.serializers import UserSerializer, UserProfileSerializer, SigninSerializer, SignUpSerializer
+from users.api.serializers import UserSerializer, UserProfileSerializer, SigninSerializer, SignUpSerializer, UserHouseSerializer12, UserHouseSerializer13, UserHouseSerializer23
 from users.api.service import UserService
 from users.api.permissions import UserPermission
 from rest_framework import permissions
@@ -12,7 +12,14 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from django.contrib.auth import login as django_login, authenticate, logout as django_logout
 from django.utils import timezone
 from department.models import Department
+from housing.models import House
+from housing.api.serializers import HouseSerializer
+from like.models import Like
+from itertools import chain
 
+def get_like_count(house_obj):
+    count = Like.objects.filter(house_id = house_obj.id).count()
+    return count
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     pagination_class = UserPagination
@@ -25,7 +32,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
         if name is not None:
             queryset = queryset.filter(name__icontains=name)
         return queryset
-
 
 class UserViewset(viewsets.ModelViewSet):
     pagination_class = UserPagination
@@ -44,6 +50,71 @@ class UserViewset(viewsets.ModelViewSet):
     def logout(self, request):
         django_logout(request)
         return Response({'next': '/'}, status=status.HTTP_200_OK)
+
+    @detail_route(methods=['GET'])
+    def suggestion(self, request, pk=None):
+        #1:close_to_department 
+        #2:cheap_house
+        #3:hot_house
+        cheap_thereshold = 600
+        count_threshold = 3
+
+        pk_int = int(pk[1:])
+        user= User.objects.raw("SELECT * FROM auth_user WHERE id = %s",[pk_int])
+        department_float = -1.0
+        for user_item in user:
+            department_float = float(user_item.profile.department.id) 
+        
+        queryset_1 = House.objects.raw('SELECT * FROM housing_house WHERE closest_department_float = %s',[department_float])
+        queryset_2 = House.objects.raw('SELECT * FROM housing_house WHERE price < %s',[cheap_thereshold])
+        
+        set_1 = set(queryset_1)
+        set_2 = set(queryset_2)
+        set_12 = set_1.intersection(set_2)
+        set_1not2 = set_1.difference(set_12)
+        set_2not1 = set_2.difference(set_12)
+        
+        queryset_i12 = list(set_12)
+        queryset_1not2 = list(set_1not2)
+        queryset_2not1 = list(set_2not1)
+
+        queryset_13_list = []
+        queryset_23_list = []
+
+        qs_none = House.objects.none()
+
+        for item in queryset_1not2:
+            if get_like_count(item) >= count_threshold:
+                queryset_13_list.append(item)
+
+        for item in queryset_2not1:
+            if get_like_count(item) >= count_threshold:
+                queryset_23_list.append(item)
+
+        queryset_i13 = list(chain(qs_none, queryset_13_list))
+        queryset_i23 = list(chain(qs_none, queryset_23_list))
+
+        serializer12 = UserHouseSerializer12(queryset_i12, many=True)
+        serializer13 = UserHouseSerializer13(queryset_i13, many=True)
+        serializer23 = UserHouseSerializer23(queryset_i23, many=True)
+        '''
+        queryset_w13_raw = House.objects.raw('SELECT * FROM housing_house WHERE closest_department_float = %s AND price >= %s',[department_float, cheap_thereshold])
+        qs_none = House.objects.none()
+        queryset_w13_filtered_list = []
+        for item in queryset_w13_raw:
+            if get_like_count(item) >= 3:
+                queryset_w13_filtered_list.append(item)
+        queryset_w13_filtered = list(chain(qs_none, queryset_w13_filtered_list))
+        serializer13 = UserHouseSerializer13(queryset_w13_filtered, many=True)
+        '''
+        Serializer_list = [serializer12.data, serializer13.data, serializer23.data]
+        content = {
+            'status': 1,
+            'responseCode' : status.HTTP_200_OK, 
+            'data': Serializer_list
+        }
+        
+        return Response(content)
 
     @list_route(methods=['POST'], permission_classes=[AllowAny])
     def signin(self, request):
