96f18b72adc77ecbd2a8af97fd309bc88ce07afd
Ao Li <aoli1996@berkeley.edu>
2018-11-23 23:13:29 -0600

    Nov23_2313

--- housing/api/serializers.py
+++ housing/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from housing.models import House
 from distance.models import Distance
+from department.models import Department
 from rest_framework.response import Response
 from django.http import JsonResponse
 from department.api.serializers import DepartmentSerializer
@@ -13,14 +14,17 @@ class HouseSerializer(serializers.ModelSerializer):
 
     def get_like_count(self, obj):
         count = Like.objects.filter(house_id = obj.id).count()
-        print(type(obj.id))
         return count
 
     def get_closest_department(self, obj):
+        department_set = Department.objects.raw('SELECT * FROM department_department WHERE id = %s', [int(obj.closest_department_float)])
         distance_set = Distance.objects.raw('SELECT * FROM distance_distance WHERE distance_distance.house_id_id = %s ORDER BY distance_distance.distance ASC',[obj.id,])
-        for item in distance_set:
-            serializer = DepartmentSerializer(item.department_id)
+        
+        for item in department_set:
+            serializer = DepartmentSerializer(item)
             serialized_data = serializer.data
+
+        for item in distance_set:
             serialized_data['distance'] = item.distance
             return serialized_data
 
